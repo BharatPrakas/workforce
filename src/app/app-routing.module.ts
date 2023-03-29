b94100ec99ac6c1e58759531a5f5a70951c56d58
Bharath Prakash <bharathprakash2395@gmail.com>
2023-03-29 13:00:31 +0530

    navbar added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/component/dashboard/dashboard.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/app/dashboard', pathMatch: 'full' },
+  {
+    path: 'app', component: NavbarComponent, children: [
+      { path: 'dashboard', component: DashboardComponent }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MediaObserver } from '@angular/flex-layout';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
-export class NavbarComponent {
-
+export class NavbarComponent implements OnInit {
+  private mediaSubscription !: Subscription;
+  isSmallDevice = false;
+  toggler = true;
+  constructor(private mediaObserver: MediaObserver) { }
+  ngOnInit(): void {
+    this.mediaSubscription = this.mediaObserver.asObservable().subscribe((x) => {
+      x.forEach(element => {
+        if (element.mqAlias == 'sm' || element.mqAlias == 'lt-sm') {
+          this.isSmallDevice = true;
+        }
+        if (element.mqAlias == 'gt-sm') {
+          this.isSmallDevice = false;
+        }
+      });
+    });
+  }
 }

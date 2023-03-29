c6f0c0a59190b52bc6f2b31e124d04bb8467e834
Bharath Prakash <bharathprakash2395@gmail.com>
2023-03-29 10:21:22 +0530

    folder structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AbsenceManagementModule } from './absence-management/absence-management.module';
+import { DashboardModule } from './dashboard/dashboard.module';
+import { InfoModule } from './info/info.module';
+import { MaterialModule } from './material/material.module';
+import { NavbarComponent } from './shared/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    AbsenceManagementModule,
+    DashboardModule,
+    InfoModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]

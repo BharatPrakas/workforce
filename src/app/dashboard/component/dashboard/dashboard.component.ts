bfd5be2df4fc5b85660a42abadeac658a08698b1
Bharath Prakash <bharathprakash2395@gmail.com>
2023-03-30 10:44:14 +0530

    dashboard update

--- src/app/dashboard/component/dashboard/dashboard.component.ts
+++ src/app/dashboard/component/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DataserviceService } from 'src/app/shared/services/dataservice.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent {
+  name!: any;
+  constructor(public db: DataserviceService) { }
 
+  ngOnInit() {
+
+
+  }
 }

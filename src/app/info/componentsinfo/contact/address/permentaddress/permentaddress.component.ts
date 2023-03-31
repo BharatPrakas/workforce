0e3f71a8284355f5c21ad660e447af687430828e
Arulmurugan300 <arulmadhujothi@gmail.com>
2023-03-31 10:29:20 +0530

    contact component created

---
+++ src/app/info/componentsinfo/contact/address/permentaddress/permentaddress.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-permentaddress',
+  templateUrl: './permentaddress.component.html',
+  styleUrls: ['./permentaddress.component.scss']
+})
+export class PermentaddressComponent {
+
+}
